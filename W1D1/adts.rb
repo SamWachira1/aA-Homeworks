8bcff599f68bd188fc6648f5943e0755446d2540
Samuel Wachira <187swachira@gmail.com>
2021-07-28 18:43:04 -0400

    completed all excercises

--- W1D1/adts.rb
+++ W1D1/adts.rb
@@ -1,26 +1,26 @@
-# class Stack LIFO
-#   def initialize
-#     @underlying_array = []
-#   end
+class Stack #LIFO
+  def initialize
+    @underlying_array = []
+  end
 
-#   def push(el)
-#     underlying_array.push(el)
-#     el
-#   end
+  def push(el)
+    underlying_array.push(el)
+    el
+  end
 
-#   def pop
-#     underlying_array.pop
-#   end
+  def pop
+    underlying_array.pop
+  end
 
-#   def peek
-#     underlying_array.last
-#   end
+  def peek
+    underlying_array.last
+  end
 
-#   private
+  private
 
-#   attr_reader :underlying_array
+  attr_reader :underlying_array
 
-# end
+end
 
 class Queue #FIFO
 
@@ -43,6 +43,53 @@ class Queue #FIFO
     end
 
 end
+
+class Map
+
+    def initialize
+        @underlying_array = []
+    end
+
+    def set(key, value)
+
+        pair_index = underlying_array.index {|pair| pair[0] == key}
+
+        if pair_index
+            underlying_array[pair_index][1] = value 
+        else
+            underlying_array.push([key,value])
+        end
+
+        value
+    end
+
+    def get(key)
+        underlying_array.each {|pair| return pair[1] if pair[0] == key}
+        nil
+    end
+
+    def delete(key)
+        value = get(key)
+        underlying_array.reject! {|pair| pair[0] == key}
+        value 
+    end
+
+    def show
+        deep_dup(underlying_array)
+    end
+
+    private
+
+    attr_reader :underlying_array
+
+    def deep_dup(arr)
+        arr.map {|el| el.is_a?(Array) ? deep_dup(el) : el }
+    end
+    
+end
+
+
+
 
 
 
