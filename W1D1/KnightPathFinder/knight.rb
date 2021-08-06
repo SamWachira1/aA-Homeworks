e6d8545b960a240650674a96d1eee8707e282b22
Samuel Wachira <187swachira@gmail.com>
2021-08-06 16:28:07 -0400

    rearranging files

--- W1D1/KnightPathFinder/knight.rb
+++ W1D1/KnightPathFinder/knight.rb
@@ -1,4 +1,19 @@
-require relative "00_tree_node"
+require_relative "00_tree_node"
+
+class KnightPathFinder
+
+    def initialize(start_pos)
+        @current_pos = start_pos
+        
+
+    end
+
+
+
+
+
+end
+
 
 
 
