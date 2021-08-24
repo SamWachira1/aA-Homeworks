0e6792b61e7a7009f84009c3e60561b1f116f589
Samuel Wachira <187swachira@gmail.com>
2021-08-24 15:16:46 -0400

    Error Handling Final

--- W1D2/Error_handling/super_useful.rb
+++ W1D2/Error_handling/super_useful.rb
@@ -1,32 +1,55 @@
-# PHASE 2
-def convert_to_int(str)
-  Integer(str)
+#PHASE 2
+
+FRUITS = ["apple", "banana", "orange"].freeze
+
+class CoffeeError < StandardError
+  def message 
+   puts "I honestly cannot have any more coffee"
+  end
 end
 
-# PHASE 3
-FRUITS = ["apple", "banana", "orange"]
+class NotCorrectFruitError < StandardError
+  def message
+   puts "Yuck, I do not eat that"
+  end
+end
+
 
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
-    raise StandardError 
+  elsif maybe_fruit == "coffee"
+    raise CoffeeError
+  else
+    raise NotCorrectFruitError
   end 
 end
 
 def feed_me_a_fruit
-  puts "Hello, I am a friendly monster. :)"
+  puts "Hello, I am a friendly monster. :)" 
 
+  begin
   puts "Feed me a fruit! (Enter the name of a fruit:)"
   maybe_fruit = gets.chomp
   reaction(maybe_fruit) 
+  rescue CoffeeError => e
+    puts e.message
+    retry
+  rescue NotCorrectFruitError => e
+    puts e.message
+    retry
+  end
 end  
 
-# PHASE 4
+# PHASE 3
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
+    raise ArgumentError.new("cannot leave name empty") if name.empty?
+    raise ArgumentError.new("best friendships take time meaning at least 5 years") if yrs_known.to_i < 5
+    raise ArgumentError.new("favorite passed time cannot be empty") if fav_pastime.empty?
+
     @name = name
-    @yrs_known = yrs_known
+    @yrs_known = yrs_known.to_i
     @fav_pastime = fav_pastime
   end
 
@@ -35,7 +58,7 @@ class BestFriend
   end
 
   def do_friendstuff
-    puts "Hey bestie, let's go #{@fav_pastime}. Wait, why don't you choose. 😄"
+    puts "Hey bestie, let's go see #{@fav_pastime}. Wait, why don't you choose. 😄"
   end
 
   def give_friendship_bracelet
--- W1D2/Error_handling/user_script.rb
+++ W1D2/Error_handling/user_script.rb
@@ -1,10 +1,9 @@
 require_relative 'super_useful'
 
-puts "'five' == #{convert_to_int('five')}"
 
 feed_me_a_fruit
 
-sam = BestFriend.new('', 1, '')
+p sam = BestFriend.new('Corey', 8, 'Mighty Ducks')
 
 sam.talk_about_friendship
 sam.do_friendstuff
