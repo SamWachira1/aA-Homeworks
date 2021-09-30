b81e2582b568b24b79ca4805097bc4e3dc411a5a
Samuel Wachira <187swachira@gmail.com>
2021-09-30 12:10:11 -0400

    updating files

--- W1D4/Hash_maps_LRU/lib/p03_hash_set.rb
+++ W1D4/Hash_maps_LRU/lib/p03_hash_set.rb
@@ -1,5 +1,5 @@
 class HashSet
-  attr_reader :count
+  attr_accessor :count, :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -7,24 +7,41 @@ class HashSet
   end
 
   def insert(key)
+    return false if include?(key)
+    self[key.hash] << key
+    self.count += 1
+
+    resize! if num_buckets < self.count
+
+    key
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    return nil unless include?(key)
+    self[key.hash].delete(key)
+    self.count -= 1
+    
   end
 
   private
-
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    self.store[num % num_buckets]
   end
 
   def num_buckets
-    @store.length
+    self.store.length
   end
 
   def resize!
+    old_store = self.store
+    self.count = 0
+    self.store = Array.new(num_buckets * 2) {Array.new}
+
+    old_store.flatten.each {|key| insert(key)}
   end
 end
