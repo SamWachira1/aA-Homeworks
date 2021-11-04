131b65950784f7ccbb89357aa20ed5d9bf482bf5
Samuel Wachira <187swachira@gmail.com>
2021-11-04 12:33:30 -0400

    updates

--- W1D4/AA_questions/question_database.rb
+++ W1D4/AA_questions/question_database.rb
@@ -8,6 +8,11 @@ class QuestionsDatabase < SQLite3::Database
          self.type_translation = true
          self.results_as_hash = true
     end
+
+    # def self.get_first_row(*args)
+    #     instance.get_first_row(*args)
+    # end
+
 end
 
 class User
@@ -20,23 +25,36 @@ class User
     end
 
     def initialize(options)
-        @id = options['id']
-        @fname = options['fname']
-        @lname = options['lname']
+        @id = options.first['id']
+        @fname = options.first['fname']
+        @lname = options.first['lname']
     end
 
     def self.find_by_id(id)
-        user_data = QuestionsDatabase.get_first_row(<<-SQL, id: id)
+        user_data = QuestionsDatabase.instance.execute(<<-SQL, id)
             SELECT
                 users.*
             FROM 
                 users
             WHERE
-                users.id = :id 
+                users.id = ?
         SQL
         user_data.nil? ? nil : User.new(user_data)
     end
 
+    def self.find_by_name(fname, lname)
+        attrs = { fname: fname, lname: lname }
+        user_data = QuestionsDatabase.get_first_row(<<-SQL, attrs)
+            SELECT
+                users.*
+            FROM 
+                users
+            WHERE 
+                users.fname = :fname AND users.lname = :lname
+         SQL
+        return nil unless user_data.length > 0  
+        User.new(user_data)
+    end
 
 
 
