08952e8e2934678c3aa511da0b7f609f2ce53ebe
Samuel Wachira <187swachira@gmail.com>
2021-12-25 20:41:54 -0500

    adding files

--- W1D5/active_record_pt1/lib/02_searchable.rb
+++ W1D5/active_record_pt1/lib/02_searchable.rb
@@ -4,9 +4,25 @@ require_relative '01_sql_object'
 module Searchable
   def where(params)
     # ...
+
+   where_line = params.keys.map {|key| "#{key} = ?"}.join("AND")
+
+    results = DbConnection.execute(<<-SQL, *params.value)
+      SELECT 
+        *
+      FROM 
+        #{table_name}
+      WHERE
+        #{where_line}
+    SQL
+
+    parse_all(results)
   end
+
+
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  # Mixin Searchable h\ere...
+  extend Searchable
 end
